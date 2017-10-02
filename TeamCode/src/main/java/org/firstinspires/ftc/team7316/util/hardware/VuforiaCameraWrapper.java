c960e82ae654bee249aea4126cfc62cb857737d3
truump <jerry21yan@gmail.com>
2017-10-02 16:47:32 -0700

    some more updates
    -chris totally wrote this

--- TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/hardware/VuforiaCameraWrapper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/hardware/VuforiaCameraWrapper.java
@@ -1,7 +1,15 @@
 package org.firstinspires.ftc.team7316.util.hardware;
 
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.team7316.util.Constants;
 import org.firstinspires.ftc.team7316.util.Loopable;
 import org.firstinspires.ftc.teamcode.R;
@@ -12,22 +20,39 @@ import org.firstinspires.ftc.teamcode.R;
 
 public class VuforiaCameraWrapper implements Loopable {
 
+    VuforiaLocalizer.Parameters params;
+    VuforiaLocalizer vuforia;
+    VuforiaTrackables glyphKeys;
+
     public VuforiaCameraWrapper() {
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.vuforiaLicenseKey = Constants.vuforiaLicenseKey;
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
 
         VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+        VuforiaTrackables glyphKeys = vuforia.loadTrackablesFromAsset("RelicVuMark");
     }
 
     @Override
     public void init() {
-
+        glyphKeys.activate();
     }
 
     @Override
     public void loop() {
+        for(VuforiaTrackable keys : glyphKeys)
+        {
+            OpenGLMatrix pose = ((VuforiaTrackableDefaultListener) keys.getListener()).getPose();
 
+            if(pose != null)
+            {
+                VectorF translation = pose.getTranslation();
+
+                Hardware.log(keys.getName() + "-Translation", translation);
+            }
+        }
     }
 
     @Override
