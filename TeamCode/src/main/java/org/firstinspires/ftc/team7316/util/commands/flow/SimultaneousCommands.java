4106142d977e527b03209e84b29789feddd02be6
Drewpew <agcummings11@gmail.com>
2017-10-17 21:52:28 -0700

    so uh this might work
    we should test it tho cause BUGS n STUFF

--- TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/commands/flow/SimultaneousCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/commands/flow/SimultaneousCommands.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.team7316.util.commands.flow;
 
 import org.firstinspires.ftc.team7316.util.Loopable;
+import org.firstinspires.ftc.team7316.util.commands.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,35 +11,33 @@ import java.util.Arrays;
  */
 public class SimultaneousCommands implements Loopable {
 
-    private ArrayList<Loopable> cmds;
+    private Command[] cmds;
 
-    public SimultaneousCommands(Loopable... cmds) {
-        this.cmds = new ArrayList<Loopable>(Arrays.asList(cmds));
+    public SimultaneousCommands(Command... cmds) {
+        this.cmds = cmds;
     }
 
     @Override
     public void init() {
-        for (Loopable cmd : this.cmds) {
-            cmd.init();
+        for (Command cmd : this.cmds) {
+            cmd.requiredSubystem().setCurrentCmd(cmd);
         }
     }
 
     @Override
     public void loop() {
-        for (int i = cmds.size()-1; i >= 0; i--) {
-            Loopable cmd = cmds.get(i);
-            cmd.loop();
 
-            if (cmd.shouldRemove()) {
-                cmd.terminate();
-                cmds.remove(i);
-            }
-        }
     }
 
     @Override
     public boolean shouldRemove() {
-        return cmds.size() <= 0;
+        for (Command cmd : cmds) {
+            if (!cmd.shouldRemove()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     @Override
