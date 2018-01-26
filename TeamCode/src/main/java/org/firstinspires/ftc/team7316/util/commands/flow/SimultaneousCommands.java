f86ae8ca8d19ac27c16c059b4ad83aba93cede99
jerukan <jerry21yan@gmail.com>
2018-01-25 17:21:23 -0800

    replaced setters AHHHHHHHHHHH

--- TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/commands/flow/SimultaneousCommands.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/commands/flow/SimultaneousCommands.java
@@ -34,7 +34,7 @@ public class SimultaneousCommands extends Command implements TerminatedListener 
 
         for (Command cmd : this.cmds) {
             Scheduler.instance.add(cmd);
-            cmd.shouldReplace = false;
+            cmd.setShouldReplace(false);
         }
     }
 
@@ -53,7 +53,7 @@ public class SimultaneousCommands extends Command implements TerminatedListener 
             subsystem.needsDefault = true;
         }
         for (Command cmd : this.cmds) {
-            cmd.shouldReplace = true;
+            cmd.setShouldReplace(true);
             cmd.terminatedListener = null;
         }
     }
