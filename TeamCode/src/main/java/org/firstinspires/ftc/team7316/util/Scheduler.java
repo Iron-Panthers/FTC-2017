89b0e1f9b43c43a623950d1be98ff51a95d1d805
Drewpew <agcummings11@gmail.com>
2018-02-01 19:02:52 -0800

    ok it was jerry's fault

--- TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/Scheduler.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/Scheduler.java
@@ -74,12 +74,16 @@ public class Scheduler {
             newCommandBuffer.remove(i);
             commands.add(newCmd);
 
-            if (newCmd.shouldBeReplaced) {
-                for (Subsystem subsystem : newCmd.requiredSubsystems) {
-                    if (subsystem.currentCmd != null) {
-                        subsystem.currentCmd.interrupt();
-                        this.commands.remove(subsystem.currentCmd);
-                    }
+            for (Subsystem subsystem : newCmd.requiredSubsystems) {
+                if (subsystem.currentCmd != null && subsystem.currentCmd.shouldBeReplaced) {
+                    subsystem.currentCmd.interrupt();
+                    this.commands.remove(subsystem.currentCmd);
+
+                    subsystem.currentCmd = newCmd;
+                }
+
+                if (subsystem.currentCmd == null) {
+                    subsystem.currentCmd = newCmd;
                 }
             }
 
