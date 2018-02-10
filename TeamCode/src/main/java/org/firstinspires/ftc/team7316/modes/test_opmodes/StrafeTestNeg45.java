cd9d88e99909378a5eeee17841e73a4cfb952d04
jerukan <jerry21yan@gmail.com>
2018-02-10 14:27:52 -0800

    some strafe tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/modes/test_opmodes/StrafeTestNeg45.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.team7316.modes.test_opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.team7316.modes.AutoBaseOpMode;
+import org.firstinspires.ftc.team7316.util.Scheduler;
+import org.firstinspires.ftc.team7316.util.commands.drive.DriveForTime;
+
+/**
+ * Created by jerry on 2/10/18.
+ */
+
+@Autonomous
+public class StrafeTestNeg45 extends AutoBaseOpMode {
+    @Override
+    public void onInit() {
+        Scheduler.instance.add(new DriveForTime(0.7, -Math.PI/4, 1));
+    }
+
+    @Override
+    public void onLoop() {
+
+    }
+}
