441b909a6d8eab389d259832600eae4514e0ce0e
Drewpew <agcummings11@gmail.com>
2018-02-22 16:54:57 -0800

    you musnt say swears
    you
    re
    on
    thin
    fuc
    king
    ice

--- TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/CryptoLocations.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/CryptoLocations.java
@@ -3,9 +3,11 @@ package org.firstinspires.ftc.team7316.util;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 
 /**
+ * The coordinate system is relative to the picto.
+ * Picto is love, picto is life. Coordinate system in millimeters and degrees.
+ *
  * Created by andrew on 2/22/18.
  */
-
 public class CryptoLocations {
 
     public static final double RED_TURN_TO_PICTO = 18;
@@ -19,21 +21,44 @@ public class CryptoLocations {
     public static RelicRecoveryVuMark PICTO_LOCATION;
     public static int AUTO_CONFIG = 0;
 
+    private static final double CLOSE_RED_LEFT_X = 100;
+    private static final double CLOSE_RED_CENTER_X = 150;
+    private static final double CLOSE_RED_RIGHT_X = 200;
+    private static final double FAR_RED_LEFT_X = 100;
+    private static final double FAR_RED_CENTER_X = 150;
+    private static final double FAR_RED_RIGHT_X = 200;
+    private static final double CLOSE_BLUE_LEFT_X = 100;
+    private static final double CLOSE_BLUE_CENTER_X = 150;
+    private static final double CLOSE_BLUE_RIGHT_X = 200;
+    private static final double FAR_BLUE_LEFT_X = 100;
+    private static final double FAR_BLUE_CENTER_X = 150;
+    private static final double FAR_BLUE_RIGHT_X = 200;
+
     /**
      * Gets delta angle needed to point at the correct box from this position.
      * CW is positive, CCW is negative.
      *
      * @param angleToPicto Angle at picto between wall and line from picto to robot
-     * @param distanceToPicto Distance from robot to picto
+     * @param xToPicto X delta from picto to robot
+     * @param yToPicto Y delta from picto to robot
      * @return A delta angle
      */
-    public static double deltaAngleForBox(double angleToPicto, double distanceToPicto) {
+    public static double deltaAngleForBox(double angleToPicto, double xToPicto, double yToPicto) {
         double x = Math.cos(0);
 
         return 0;
     }
 
-    public static double distanceForBox(double angleToPicto, double distanceToPicto) {
+    /**
+     * Gets distance needed to move from this position to correct column.
+     * Robot is placed in front of box so that you need only to outtake.
+     *
+     * @param angleToPicto Angle at picto between wall and line from picto to robot
+     * @param xToPicto X delta from picto to robot
+     * @param yToPicto Y delta from picto to robot
+     * @return A delta angle
+     */
+    public static double distanceForBox(double angleToPicto, double xToPicto, double yToPicto) {
         return 0;
     }
 
