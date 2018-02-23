30f120267d130dc0c6afa4285155b92c5b2058fa
Drewpew <agcummings11@gmail.com>
2018-02-22 16:28:09 -0800

    use this to win :;)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/CryptoLocations.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.team7316.util;
+
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+
+/**
+ * Created by andrew on 2/22/18.
+ */
+
+public class CryptoLocations {
+
+    public static RelicRecoveryVuMark PICTO_LOCATION;
+    public static double deltaAngleForBox(double angleToPicto, double distanceToPicto) {
+
+    }
+
+    public static double distanceForBox(double angleToPicto, double distanceToPicto) {
+
+    }
+
+}
