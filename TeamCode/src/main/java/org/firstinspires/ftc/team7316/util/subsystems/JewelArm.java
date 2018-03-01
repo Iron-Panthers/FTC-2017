07a27a112b5e934b271d11bee391b025c69ca447
jerukan <jerry21yan@gmail.com>
2018-02-28 16:33:55 -0800

    jewel arm no break anymore

--- TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/subsystems/JewelArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/team7316/util/subsystems/JewelArm.java
@@ -67,7 +67,7 @@ public class JewelArm extends Subsystem {
                 wacker.setPosition(wackPositionNeutral);
                 break;
             case INITIAL:
-                wacker.setPosition(wackPositionInit);
+                wacker.setPosition(wackPositionNeutral);
         }
     }
 
